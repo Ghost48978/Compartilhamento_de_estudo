f6675e04569933c5ef1644ed5db1d6287163c768
MateusCoelho023 <capic.2022118isinf0152@aluno.ifpi.edu.br>
2023-10-18 11:23:45 -0300

    Primeiro index

--- projeto/polls/views.py
+++ projeto/polls/views.py
@@ -1,5 +1,19 @@
 from django.http import HttpResponse
-
+from .models import Question
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Rafael um aumento por favor")
+    questions=Question.objects.all()
+    context={"questions":questions}
+    return render(request,"polls/index.html",context)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s"% question_id)
+
+def results(request,question_id):
+    response="You're looking at the results of questions %s."
+    return HttpResponse(response %question_id)
+
+def vote (request, question_id):
+    return HttpResponse("You're voting on question %s.")
+
